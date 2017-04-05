227c0d7e334e1dea31efcf7988cf0db4ef9b87a0
Meltem <melltemkillic@gmail.com>
2017-04-05 12:49:40 -0400

    character counting exercise is done

---
+++ character-counting.js
@@ -0,0 +1,18 @@
+function countLetters(sentence) {
+  var obj = {};
+  sentence = sentence.split(' ').join('');
+  console.log(sentence);
+  for (var i in sentence) {
+    var letter = sentence.charAt(i);
+    if(!obj[letter]) {
+      obj[letter] = 1;
+    } else {
+      obj[letter] ++;
+    }
+  }
+  return obj;
+}
+
+var a = countLetters("lighthouse in the house");
+
+console.log(a);
