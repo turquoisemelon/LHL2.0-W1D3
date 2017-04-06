8fd11998bdf005dd4ea0b4090d67fd6c0e9342e4
Meltem <melltemkillic@gmail.com>
2017-04-05 21:10:47 -0400

    the functions inside library.js file embeded inside the library object and added to a new file. Code has been modified and this used

--- music-library/music-library-2.js
+++ music-library/music-library-2.js
@@ -20,11 +20,53 @@ var library = {
                       name: "Other Playlist",
                       tracks: ["t03"]
                     }
-             }
-  printPlaylists : function () {
-               for (var elem in this.playlists) {
-                 var result = `${elem}: ${this.playlists[elem].name} - ${this.playlists[elem]["tracks"].length} tracks`;
-                 console.log(result);
-               }
-             }
+             },
+  printPlaylists:  function () {
+                    for (var elem in this.playlists) {
+                       var result = `${elem}: ${this.playlists[elem].name} - ${this.playlists[elem]["tracks"].length} tracks`;
+                       console.log(result);
+                    }
+                  },
+  printTracks:  function () {
+                    for (var elem in this.tracks) {
+                       var result = `${elem}: ${this.tracks[elem].name} by ${this.tracks[elem].artist} (${this.tracks[elem].album})`;
+                       console.log(result);
+                    }
+                },
+  printPlaylist: function (playlistId) {
+                  var resultPlaylist = `${playlistId}: ${this.playlists[playlistId]["name"]} - ${this.playlists[playlistId]["tracks"].length} tracks`;
+                  console.log(resultPlaylist);
+                  for (var track of this.playlists[playlistId]["tracks"]){
+                    var resultTracks = `${track}: ${this.tracks[track].name} by ${this.tracks[track].artist} (${this.tracks[track].album})`;
+                    console.log(resultTracks);
+                  }
+                },
+  addTrackToPlaylist: function(trackId, playlistId) {
+    
+  },
+  addTrack: function (name, artist, album) {
+              var newTrack = `t${uid()}`;
+              this.tracks[newTrack] = {"id": newTrack, "name": name, "artist": artist, "album": album};
+              console.log(this.tracks);
+              return this.tracks;
+            },
+  addPlaylist: function (name) {
+                 var newPlaylist = `p${uid()}`;
+                 var newTrack = `t${uid()}`;
+                 var newTrackArr = [];
+                 newTrackArr.push(newTrack);
+                 this["playlists"][newPlaylist] = {"id": newPlaylist, "name": name, "tracks": newTrackArr};
+                 console.log(this.playlists);
+                 return this.playlists;
+              }
 }
+
+var uid = function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
+
+library.printPlaylists();
+library.printTracks();
+library.printPlaylist('p01');
+library.addTrack('foo','someone', 'something');
+library.addPlaylist('foo');
